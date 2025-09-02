427de3d54b5d32e809f6e9888e3e7e7adc2c62e7
Cecil1999 <Cecil2090@gmail.com>
2025-09-01 21:18:18 -0400

    Add ContactMailer, get working form and some validation for email.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+    before_action :page_params, only: %i[ email ]
+
     def home
     end
 
@@ -12,9 +14,22 @@ class PagesController < ApplicationController
     end
 
     def email
+        @email_info = params
+
+        if @email_info['email'] !~ URI::MailTo::EMAIL_REGEXP
+            flash['alert'] = 'Please use a valid email.'
+        else
+            flash['notice'] = 'Email has been sent.'
+            ContactMailer.contact_email(@email_info['name'], @email_info['message'], @email_info['email']).deliver_later
+        end
+
+        redirect_to contact_path
     end
 
     private
 
+    def page_params
+        params.expect(:email, :name, :message)
+    end
 
 end
